980f23e24c6e893e2f2115c3102bc521a2603642
Oleksandr Revva <revvao@gmail.com>
2022-12-14 17:17:40 +0200

    task 1 and task 2

---
+++ js/01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+console.log(galleryItems);
+
+const imagesContainer = document.querySelector('.gallery');
+const imagesMarkup = createCardsImagesMarkup(galleryItems);
+
+imagesContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+imagesContainer.addEventListener('click', onImageContainerClick);
+
+function createCardsImagesMarkup(items) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+function onImageContainerClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`,
+
+    {
+      onShow: () => document.addEventListener('keydown', onEscKeyPress),
+      onClose: () => document.removeEventListener('keydown', onEscKeyPress),
+    }
+  );
+  instance.show();
+  function onEscKeyPress(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
